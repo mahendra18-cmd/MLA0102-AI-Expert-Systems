5d7a7cdd11616839089ab783ece499315fdd71af
MAHENDRA REDDY <192425261.simats@saveetha.com>
2025-10-16 13:44:40 +0530

    Add files via upload

---
+++ exp 9.py
@@ -0,0 +1,12 @@
+import math
+
+def sigmoid(x): return 1/(1+math.exp(-x))
+
+def xor(x1, x2):
+    h1 = sigmoid(x1*20 + x2*(-20) - 10)
+    h2 = sigmoid(x1*(-20) + x2*20 - 10)
+    out = sigmoid(h1*20 + h2*20 - 10)
+    return round(out)
+
+for a,b in [(0,0),(0,1),(1,0),(1,1)]:
+    print(a,b,"→",xor(a,b))
